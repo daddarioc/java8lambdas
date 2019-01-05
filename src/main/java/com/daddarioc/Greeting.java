6d7ee6317e139e2f677637020aea1f03668249c4
daddarioc <daddarioc@gmail.com>
2019-01-05 12:21:38 -0500

    example showing a lambda in place of an anonymous inner class when creating a new thread

--- src/main/java/com/daddarioc/Greeting.java
+++ src/main/java/com/daddarioc/Greeting.java
@@ -1,6 +1,7 @@
 
 package com.daddarioc;
 
+@FunctionalInterface    // gives clue to any implementer that this is meant for a lambda
 public interface Greeting {
     void perform();
 }
---
+++ src/main/java/com/daddarioc/RunnableExample.java
@@ -0,0 +1,26 @@
+package com.daddarioc;
+
+public class RunnableExample {
+
+    public static void main(String[] args) {
+
+        // new thread using anon inner class
+        Thread myThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("printed inside Runnable");
+            }
+        });
+
+        myThread.run();
+
+        // this is perfect candidate to rewrite as a lambda
+        // since lambdas are based on interfaces, the thread constructor
+        // is none the wiser as to what was sent into it
+        Thread myThread2 = new Thread(() -> System.out.print("printed from lambda"));
+
+        myThread2.run();
+
+
+    }
+}
